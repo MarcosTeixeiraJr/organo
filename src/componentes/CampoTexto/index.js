cd0370dc8d4f645a6e69e8c1bdcac9ddfd7a846f
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-12-24 18:32:06 -0300

    Projeto Finalizado

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,18 @@
+import'./CampoTexto.css';
+
+
+const CampoTexto = (props) => {
+
+    const aoDigitdo = (evento) => {
+       props.aoAlterado(evento.target.value)
+    }
+ 
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input value={props.valor} onChange={aoDigitdo} required={props.obrigatorio} placeholder={`${props.placeholder}...`} />
+        </div>
+    )
+}
+
+export default CampoTexto;
