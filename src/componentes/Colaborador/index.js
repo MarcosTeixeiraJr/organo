43e9553bfd1d72b06a7a541625eddcb2b5208a94
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-12-27 15:40:14 -0300

    botão deletar e input de cor

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,16 @@
-import './Colaborador.css';
+import { IoMdCloseCircle } from "react-icons/io";
+import './colaborador.css'
 
-const Colaborador = ({nome, imagem, cargo, corDeFundo}) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (
-        <div className='colaborador'>
-            <div className='cabecalho' style={{backgroundColor: corDeFundo}}>
-                <img src={imagem} alt={nome} />
+        <div className="colaborador">
+            <IoMdCloseCircle size={20} className='deletar' onClick={aoDeletar}/>
+            <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+                <img src={colaborador.imagem} alt={colaborador.nome} />
             </div>
-            <div className='rodape'>
-                <h4>{nome}</h4>
-                <h5>{cargo}</h5>
+            <div className="rodape">
+                <h4>{colaborador.nome}</h4>
+                <h5>{colaborador.cargo}</h5>
             </div>
         </div>
     );
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,27 +1,20 @@
-import './Time.css';
-import Colaborador from '../Colaborador';
+import Colaborador from '../Colaborador'
+import './time.css'
 
-
-const Time = (props) => {
-    const css = { backgroundColor: props.corSecundaria };
-    
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
-        (props.colaboradores.length > 0) ? <section className="time" style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.corSecundaria} type='color' className='input-cor' />
+            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador
-                    corDeFundo={props.corPrimaria}
-                    key={colaborador.nome}
-                    nome={colaborador.nome}
-                    cargo={colaborador.cargo}
-                    imagem={colaborador.imagem}
-                    />)}
+                {colaboradores.map((colaborador, indice) => {
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />;
+                })}
             </div>
         </section>
-        : ''
-    );
+
+    )
 }
 
-export default Time;
-
-
+export default Time
