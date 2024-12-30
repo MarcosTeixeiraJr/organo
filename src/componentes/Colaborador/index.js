ad62b15423d6c4c5d478050670965b3e7bba39e7
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-12-30 16:11:06 -0300

    Conclusão do Projeto

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,15 @@
-import { IoMdCloseCircle } from "react-icons/io";
+import { IoMdCloseCircle, IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar(){
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsfavorito = {
+        size:25,
+        onClick: favoritar
+    }
     return (
         <div className="colaborador">
             <IoMdCloseCircle 
@@ -15,6 +23,12 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
             <div className="rodape">
                 <h4>{colaborador.nome}</h4>
                 <h5>{colaborador.cargo}</h5>
+                <div className="favoritar">
+                    {colaborador.favorito
+                        ? <IoMdHeart { ...propsfavorito } color='#ff0000' />   
+                        : <IoMdHeartEmpty { ...propsfavorito } /> 
+                    }
+                </div>
             </div>
         </div>
     );
